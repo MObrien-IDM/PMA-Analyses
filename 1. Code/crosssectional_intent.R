0755c18b18a97493d5e5e0d3bf09fa1be3d80825
MObrien-IDM <79864181+MObrien-IDM@users.noreply.github.com>
2024-09-12 16:24:34 -0700

    Add near term intent coding for IPUMS formatting

--- 1. Code/crosssectional_intent.R
+++ 1. Code/crosssectional_intent.R
@@ -19,14 +19,20 @@ if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr pack
 ddi <- read_ipums_ddi("pma_00002.xml")
 data <- read_ipums_micro(ddi)
 
+pma_cs <- as.data.frame(data)
+
 ### Replicate code for near-term intent from longitudinal analysis
-pma$near_term_intent <- NA
-pma$near_term_intent[pma$fp_start == "1. X months" & pma$fp_start_value <= 12] <- "Near-term intent"
-pma$near_term_intent[pma$fp_start == "2. X years" & pma$fp_start_value <= 1] <- "Near-term intent"
-pma$near_term_intent[pma$fp_start == "-88. Do not know"] <- "Desire to use in future"
-pma$near_term_intent[pma$fp_start == "3. Soon/now"] <- "Near-term intent"
-pma$near_term_intent[pma$fp_start == "4. After the birth of this child"] <- "Desire to use in future"
-pma$near_term_intent[pma$future_user_not_current == "0. No"] <- "No intent to use in future"
-pma$near_term_intent[pma$current_user == "1. Yes"] <- "Current user"
-pma$near_term_intent[pma$pregnant == "1. Yes"] <- "Pregnant"
+pma_cs$near_term_intent <- NA
+pma_cs$near_term_intent[pma_cs$FPPLANWHEN == 01 & pma_cs$FPPLANVAL <= 12] <- "Near-term intent" ## less than or equal 12 months
+pma_cs$near_term_intent[pma_cs$FPPLANWHEN == 02 & pma_cs$fp_start_value <= 1] <- "Near-term intent" ## less than or equal 1 year
+pma_cs$near_term_intent[pma_cs$FPPLANWHEN == 03] <- "Near-term intent"
+pma_cs$near_term_intent[pma_cs$FPPLANWHEN == 04] <- "Some time in future"
+pma_cs$near_term_intent[pma_cs$FPPLANWHEN == 97] <- "Some time in future"
+pma_cs$near_term_intent[pma_cs$FPUSPLAN == 00] <- "No intent to use in future"
+pma_cs$near_term_intent[pma_cs$FPUSPLAN == 99] <- "Not in universe"
+pma_cs$near_term_intent[pma_cs$FPUSPLAN == 98] <- "No response/missing"
+
+## Weighted descriptives 
+library(questionr)
+wtd.table(x = pma_cs$near_term_intent, weights = pma_cs$FQweight)
 
